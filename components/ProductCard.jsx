d2132f57d9257d738d6233f79bfadce125331425
rezasdsju <rezaul2002104@gmail.com>
2026-07-14 19:34:16 +0600

    src url has been corrected in img tag

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,7 +1,8 @@
 function ProductCard({product}) {
+    const BASE_URL = import.meta.env.VITE_DJANGO_BASE_URL;
     return (
-        <div className="bg-white rounded-xl shadow-lg overflow-hidden max-w-sm hover:shadow-xl transition-shadow duration-300">
-            {/* <img src="{product.image}" alt="{product.name}" className="w-full h-56 object-cover rounded-lg mb-4" /> */}
+        <div className="bg-white rounded-xl shadow-lg overflow-hidden max-w-lg hover:shadow-xl transition-shadow duration-300">
+            <img src={`${BASE_URL}${product.image}`} alt={product.name} className="w-full h-56 object-cover rounded-lg mb-4" />
             <h2 className="text-lg font-semibold mb-2 text-gray-800 truncate">{product.name}</h2>
             <p className="text-gray-600 font-medium">${Number(product.price).toFixed(2)}</p>
         </div>
